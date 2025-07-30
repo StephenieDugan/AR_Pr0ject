e64f4162cb7a60f245fcf46ff3bd35d58035caa9
StephenieDugan <stepheniedugan617@gmail.com>
2025-07-30 12:44:11 -0600

    ProduceInteractor Edits

--- Assets/Scripts/PlantInteracterScript.cs
+++ Assets/Scripts/PlantInteracterScript.cs
@@ -1,29 +1,31 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems; // Required for IPointerDownHandler, IDragHandler, IEndDragHandler
+using UnityEngine.XR.ARFoundation; // Required for ARRaycastManager
+using UnityEngine.XR.ARSubsystems;
 
-public class PlantInteracterScript : MonoBehaviour
+public class PlantInteracterScript : MonoBehaviour, IPointerDownHandler
 {
 	[SerializeField] private LayerMask whoIHit;
+	[SerializeField] private Camera cam;
 
-	private void Update()
-	{
-		if (Input.touchCount > 0)
-		{
-			Touch t = Input.GetTouch(0);
-			if (t.phase == TouchPhase.Began)
-			{
-				RaycastHit hit;
-				Ray raypos = Camera.main.ScreenPointToRay(t.position);
-				Physics.Raycast(raypos, out hit, 1000.0f, whoIHit);
-				Debug.DrawRay(raypos.origin, raypos.direction, Color.aliceBlue, 5.0f);
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        RaycastHit[] hit;
+        Ray raypos = Camera.main.ScreenPointToRay(eventData.position);
+        hit = Physics.RaycastAll(raypos, 1000.0f, whoIHit);
+        Debug.DrawRay(raypos.origin, raypos.direction, Color.aliceBlue, 5.0f);
 
 
-				if (hit.rigidbody)
-				{
-					ProduceScript produceHit = hit.rigidbody.GetComponent<ProduceScript>();
-
-					produceHit.TryHarvest();
-				}
-			}
-		}
-	}
+        foreach (var item in hit)
+        {
+            if (item.rigidbody)
+            {
+                if (item.rigidbody.GetComponent<ProduceScript>())
+                {
+                    item.rigidbody.GetComponent<ProduceScript>().TryHarvest();
+                }
+            }
+        }
+    }
 }
